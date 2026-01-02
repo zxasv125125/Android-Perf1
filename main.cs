3221b6e19c341f4dc6e842f55ef5fb4c65a548a7
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-01-02 10:52:46 +0700

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,20 +1,7 @@
-using System;
-using StardewModdingAPI;
-
 namespace AndroidPerf;
 
 public class AndroidPerf : Mod
-namespace AndroidPerf
-{
-	public override void Entry(IModHelper helper)
+public override void Entry(IModHelper helper)
 	{
 		((Mod)this).Monitor.Log("Using AndroidPerf", (LogLevel)2);
-	}
-    public class AndroidPerf : Mod
-    {
-        public override void Entry(IModHelper helper)
-        {
-            this.Monitor.Log("AndroidPerf Dummy Loaded", LogLevel.Info);
-        }
-    }
 }
